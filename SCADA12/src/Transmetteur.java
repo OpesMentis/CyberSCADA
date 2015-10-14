a81a6f66e2251d083eafc72b56470ee262193f0a
OpesMentis <planchot.antoine@gmail.com>
2015-10-14 23:13:15 +0200

    Le projet eclipse complet.

---
+++ SCADA12/src/Transmetteur.java
@@ -0,0 +1,13 @@
+public class Transmetteur {
+	public void eventBarriere(boolean etat, int ref) { // etat true = ouverte, false = fermee
+		
+	}
+	
+	public void eventBateau(int pos) {
+		
+	}
+	
+	public void start(String dest) {
+		
+	}
+}
